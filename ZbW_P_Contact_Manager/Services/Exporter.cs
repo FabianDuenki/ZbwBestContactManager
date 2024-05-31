55c8f4fd01341bc427b4e4e2b27ee5e2705573f4
Beyluta <beyluta1@gmail.com>
2024-05-31 19:24:07 +0200

    fix csv formatting

--- ZbW_P_Contact_Manager/Services/Exporter.cs
+++ ZbW_P_Contact_Manager/Services/Exporter.cs
@@ -4,33 +4,69 @@ using System.Text;
 
 namespace Services
 {
-    public class Exporter<T>(T? instance) where T : class
+    public class Exporter<T> where T : class
     {
+        private T[]? Instances { get; set; }
+        private T? Instance { get; set; }
+
         public enum ExportType
         {
             CSV
         }
 
-        private T? Instance { get; set; } = instance;
+        public Exporter(T? instance)
+        {
+            Instance = instance;
+        }
+
+        public Exporter(T[]? instances)
+        {
+            Instances = instances;
+        }
 
         public ExportStatus Export(ExportType type)
         {
-            if (Instance is null) return ExportStatus.Error;
-            string result = GetFormattedInstanceData(type);
-            string fileName = $"{new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()}.{type.ToString().ToLower()}";
-            string filePath = Path.Combine(SystemFolders.GetPath(SystemFolders.Folder.Downloads), fileName);
-            CreateFile(result, filePath);
-            return ExportStatus.Success;
+            if (Instances is not null) return MultipleExport(type, Instances);
+            if (Instance is not null) return SingleExport(type, Instance);
+            return ExportStatus.Error;
         }
 
-        private string GetFormattedInstanceData(ExportType type)
+        private ExportStatus SingleExport(ExportType type, T instance)
+        {
+            return SaveFileByType(type, string.Join(",", instance.GetType()
+                .GetProperties()
+                .Select(propertyInfo => propertyInfo.Name)) + $"\n{GetFormattedData(type, instance)}");
+        }
+
+        private ExportStatus MultipleExport(ExportType type, T[] instances)
+        {
+            return SaveFileByType(type, GetFormattedHeaders(type, instances.First()) + "\n" + string.Join("\n", instances
+                .Select(instance => GetFormattedData(type, instance))));
+        }
+
+        private string GetFormattedData(ExportType type, T instance)
         {
             return type switch
             {
-                ExportType.CSV or _ => string.Join("\n", Instance!.GetType()
+                ExportType.CSV or _ => string.Join(",", instance.GetType()
                     .GetProperties()
-                    .Select(propertyInfo => $"{propertyInfo.Name},{propertyInfo.GetValue(Instance)}")),
+                    .Select(propertyInfo => propertyInfo.GetValue(instance))),
             };
+        }
+
+        private string GetFormattedHeaders(ExportType type, T instance)
+        {
+            return type switch
+            {
+                ExportType.CSV or _ => string.Join(",", instance.GetType().GetProperties().Select(propertyInfo => propertyInfo.Name))
+            };
+        }
+
+        private ExportStatus SaveFileByType(ExportType type, string content)
+        {
+            string fileName = $"{new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()}.{type.ToString().ToLower()}";
+            string filePath = Path.Combine(SystemFolders.GetPath(SystemFolders.Folder.Downloads), fileName);
+            return CreateFile(content, filePath) == FileStatus.Success ? ExportStatus.Success : ExportStatus.Error;
         }
 
         public FileStatus CreateFile(string content, string path)
