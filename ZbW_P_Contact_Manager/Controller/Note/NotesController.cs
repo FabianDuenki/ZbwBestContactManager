850591939d10fe4e468a6fbeb550e8f03fdac008
janahuwiler <huwiler.jana.gmail.com@zbw-online.ch>
2024-06-02 23:00:39 +0200

    add function and rename variable

--- ZbW_P_Contact_Manager/Controller/Note/NotesController.cs
+++ ZbW_P_Contact_Manager/Controller/Note/NotesController.cs
@@ -11,17 +11,29 @@ namespace Controller
             this.notes = notes;
         }
 
-        public void CreateNote(long personId, string text, string createdBy)
+        public void CreateNote(long personId, string comment, string createdBy)
         {
             var note = new Note()
             {
                 Id = new Guid(),
-                Comment = text,
+                Comment = comment,
                 PersonId = personId,
                 CreatedBy = createdBy,
                 CreatedAt = DateTime.Now
             };
             notes.Add(note);
+        }
+
+        public void UpdateNote(long personId, Guid noteId, string newComment, string updatedBy)
+        {
+            var note = notes.Find(n => n.PersonId == personId && n.Id == noteId);
+
+            if (note != null)
+            {
+                note.Comment = newComment;
+                note.CreatedBy = updatedBy;
+                note.CreatedAt = DateTime.Now;
+            }
         }
 
         public void DeleteNotes(long personId)
