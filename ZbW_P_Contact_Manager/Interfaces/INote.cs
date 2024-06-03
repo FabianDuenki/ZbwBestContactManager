e70f1e1eeaaf7e5a203cc96cc707da0da51c3b46
Beyluta <beyluta1@gmail.com>
2024-06-03 20:36:44 +0200

    add note interface

---
+++ ZbW_P_Contact_Manager/Interfaces/INote.cs
@@ -0,0 +1,19 @@
+﻿namespace Interface
+{
+    public interface INote
+    {
+        public Guid? Id { get; set; }
+
+        public string? Comment { get; set; }
+
+        public long? PersonId { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string? UpdatedBy { get; set; }
+    }
+}
