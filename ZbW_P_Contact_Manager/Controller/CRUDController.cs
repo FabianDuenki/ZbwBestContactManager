5f8a425011d89614e7807f731d1adc785dd12d7c
Fabian Duenki <fabian.duenki@buhlergroup.com>
2024-06-04 15:42:39 +0200

    handling new Person object

--- ZbW_P_Contact_Manager/Controller/CRUDController.cs
+++ ZbW_P_Contact_Manager/Controller/CRUDController.cs
@@ -1,12 +1,10 @@
 ﻿using Model;
 using Services;
-using Struct;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Services;
 using System.Reflection.Metadata;
 using External;
 using Microsoft.VisualBasic.FileIO;
@@ -15,9 +13,9 @@ namespace Controller
 {
     internal class CRUDController
     {
-        public static Person CreatePerson(PersonDetails person)
+        public static Person CreatePerson(Person person)
         {
-            var exporter = new Exporter<Person>(new Person(person));
+            var exporter = new Exporter<Person>(person);
             string fileName = person.GetType().Name + ".csv";
 
             if (string.IsNullOrEmpty(person.Salutation))
@@ -42,23 +40,28 @@ namespace Controller
 
             if (!File.Exists(fileName))
             {
-                File.WriteAllText(fileName, CreateCsvHeader());
+                File.WriteAllText(fileName, CsvHeader());
             }
 
             AppendFile(fileName, person);
 
-            return new Person(person);
+            return person;
         }
-        public static Person ReadPerson(PersonDetails person)
+        public static Person ReadPerson(Person person)
         {
-            return new Person(person);
+            return person;
         }
-        public static Person UpdatePerson(PersonDetails person)
+        public static Person UpdatePerson(Person person)
         {
-            return new Person(person);
+            return person;
         }
-        public static bool DeletePerson(PersonDetails person)
+        public static bool DeletePerson(Person person)
         {
+            if(person.FirstName == null || person.LastName == null)
+            {
+                throw new ArgumentException("Please Enter the lastname and firstname of the person to be deleted");
+            }
+
             string fileName = person.GetType().Name + ".csv";
 
             string[] fileContent = File.ReadAllLines(fileName);
@@ -74,21 +77,21 @@ namespace Controller
             }
             return true;
         }
-        private static void AppendFile(string fileName, PersonDetails person)
+        private static void AppendFile(string fileName, Person person)
         {
             try
             {
-                File.AppendAllText(fileName, PersonToString(person));
+                File.AppendAllText(fileName, PersonToCsvString(person));
             }
             catch (IOException)
             {
                 Console.WriteLine("ALTER, schlüss das fucking file im Excel");
-                Thread.Sleep(5000);
+                Thread.Sleep(500);
                 AppendFile(fileName, person);
             }
             return;
         }
-        private static string CreateCsvHeader()
+        private static string CsvHeader()
         {
             return 
                 "\"Salutation\"," +
@@ -109,7 +112,7 @@ namespace Controller
                 "\"ZipCode\"," +
                 "\"Place\"," + Environment.NewLine;
         }
-        private static string PersonToString(PersonDetails person)
+        private static string PersonToCsvString(Person person)
         {
             return
                 person.Salutation + "," +
