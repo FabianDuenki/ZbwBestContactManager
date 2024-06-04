69d821fffca3b16bbc0bc7feb7b4012d0ddaf856
Fabian Duenki <fabian.duenki@buhlergroup.com>
2024-06-04 15:43:13 +0200

    gender is nonbinary hehe

--- ZbW_P_Contact_Manager/Models/Person.cs
+++ ZbW_P_Contact_Manager/Models/Person.cs
@@ -8,7 +8,7 @@ namespace Model
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        public bool? Gender { get; set; }
+        public string? Gender { get; set; }
         public string? Title { get; set; }
         public string? SocialSecurityNumber { get; set; }
         public string? PhoneNumberPrivate { get; set; }
