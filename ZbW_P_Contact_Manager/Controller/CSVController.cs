0075c67f3b5749ce95e6b5484614ad94bd0ab7cf
Pedro Ribeiro <beyluta1@gmail.com>
2024-06-04 19:15:02 +0200

    add method definitions

--- ZbW_P_Contact_Manager/Controller/CSVController.cs
+++ ZbW_P_Contact_Manager/Controller/CSVController.cs
@@ -2,71 +2,81 @@
 using Model.Operation;
 using System.Text;
 
-namespace Services
+namespace Controller
 {
-    public class Exporter<T> where T : class
+    public class CSVController<T> where T : class
     {
         private T[]? Instances { get; set; }
         private T? Instance { get; set; }
 
-        public enum ExportType
+        public enum Format
         {
             CSV
         }
 
-        public Exporter(T? instance)
+        public CSVController(T? instance)
         {
             Instance = instance;
         }
 
-        public Exporter(T[]? instances)
+        public CSVController(T[]? instances)
         {
             Instances = instances;
         }
 
-        public ExportStatus Export(ExportType type)
+        public FormatStatus Export(Format type)
         {
             if (Instances is not null) return MultipleExport(type, Instances);
             if (Instance is not null) return SingleExport(type, Instance);
-            return ExportStatus.Error;
+            return FormatStatus.Error;
         }
 
-        private ExportStatus SingleExport(ExportType type, T instance)
+        public FormatStatus Import(Format type, string content)
+        {
+            return FormatStatus.Error;
+        }
+
+        public FormatStatus MultipleImport(Format type, string content)
+        {
+            return FormatStatus.Error;
+        }
+
+        private FormatStatus SingleExport(Format type, T instance)
         {
             return SaveFileByType(type, string.Join(",", instance.GetType()
                 .GetProperties()
                 .Select(propertyInfo => propertyInfo.Name)) + $"\n{GetFormattedData(type, instance)}");
         }
 
-        private ExportStatus MultipleExport(ExportType type, T[] instances)
+        private FormatStatus MultipleExport(Format type, T[] instances)
         {
             return SaveFileByType(type, GetFormattedHeaders(type, instances.First()) + "\n" + string.Join("\n", instances
                 .Select(instance => GetFormattedData(type, instance))));
         }
 
-        private string GetFormattedData(ExportType type, T instance)
+        private string GetFormattedData(Format type, T instance)
         {
             return type switch
             {
-                ExportType.CSV or _ => string.Join(",", instance.GetType()
+                Format.CSV or _ => string.Join(",", instance.GetType()
                     .GetProperties()
                     .Select(propertyInfo => propertyInfo.GetValue(instance))),
             };
         }
 
-        private string GetFormattedHeaders(ExportType type, T instance)
+        private string GetFormattedHeaders(Format type, T instance)
         {
             return type switch
             {
-                ExportType.CSV or _ => string.Join(",", instance.GetType().GetProperties().Select(propertyInfo => propertyInfo.Name))
+                Format.CSV or _ => string.Join(",", instance.GetType().GetProperties().Select(propertyInfo => propertyInfo.Name))
             };
         }
 
-        private ExportStatus SaveFileByType(ExportType type, string content)
+        private FormatStatus SaveFileByType(Format type, string content)
         {
             string fileName = $"{new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()}.{type.ToString().ToLower()}";
             string filePath = Path.Combine(SystemFolders.GetPath(SystemFolders.Folder.Downloads), fileName);
-            return CreateFile(content, filePath) == FileStatus.Success ? ExportStatus.Success : ExportStatus.Error;
+            return CreateFile(content, filePath) == FileStatus.Success ? FormatStatus.Success : FormatStatus.Error;
         }
 
         public FileStatus CreateFile(string content, string path)
@@ -80,7 +90,7 @@ namespace Services
                 }
 
                 return FileStatus.Success;
-            } 
+            }
             catch { return FileStatus.Error; }
         }
 
