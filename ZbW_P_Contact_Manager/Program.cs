edce4302b5c935efd7df1cbb13637873b1e15c4d
janahuwiler <huwiler.jana.gmail.com@zbw-online.ch>
2024-06-18 13:56:31 +0200

    change main form and move notes into UI folder

--- ZbW_P_Contact_Manager/Program.cs
+++ ZbW_P_Contact_Manager/Program.cs
@@ -1,3 +1,8 @@
+using Controller;
+using Model;
+using ZbW_P_Contact_Manager;
+using ZbW_P_Contact_Manager.UI;
+
 namespace View
 {
     internal static class Program
@@ -11,7 +16,7 @@ namespace View
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
