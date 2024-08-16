9a4df77938758318efdd0b1905f59fa1ed3068bb
Pedro Ribeiro <beyluta1@gmail.com>
2024-08-15 19:47:06 -0700

    add file handler class

---
+++ ZbW_P_Contact_Manager/External/FileHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace External
+{
+    public static class FileHandler
+    {
+        public static string GetFilePathFromPicker()
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                InitialDirectory = "C:\\",
+                Filter = "Text|*.txt|All|*.*",
+                FilterIndex = 0,
+                RestoreDirectory = true
+            };
+            return dialog.ShowDialog() == DialogResult.OK ? dialog.FileName : string.Empty;
+        }
+    }
+}
