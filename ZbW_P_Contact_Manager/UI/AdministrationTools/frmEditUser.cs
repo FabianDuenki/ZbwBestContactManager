d1cdd23a341f03c42543251d14719b0bca05e00f
Fabian Dünki <fabian-duenki@outlook.com>
2024-09-08 21:24:06 +0200

    update user in UI

--- ZbW_P_Contact_Manager/UI/AdministrationTools/frmEditUser.cs
+++ ZbW_P_Contact_Manager/UI/AdministrationTools/frmEditUser.cs
@@ -1,186 +1,191 @@
-﻿using System;
+﻿using Model;
+using System;
+using System.CodeDom;
 using System.Windows.Forms;
+using UI.AdministrationTools.Classes;
+using Controller;
+using Model.Typing;
+using OpenTK.Platform.Windows;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace ZbW_P_Contact_Manager.UI.AdministrationTools
 {
     public partial class frmEditUser : Form
     {
-        public frmEditUser()
+        Person _currentUser;
+        Person _updatedUser;
+        UserController _userController = new UserController();
+        ModelController _modelController = new ModelController();
+
+        public frmEditUser(Person user)
         {
+            _currentUser = user;
+
             InitializeComponent();
+            ActivateTextFields();
+            FillTextFields();
+        }
+        private void ActivateTextFields()
+        {
+            btnTraineeYearsEdit.Enabled = false;
 
-            // Add event handlers for each TextBox TextChanged event
-            txtSalutation.TextChanged += new EventHandler(txtSalutation_TextChanged);
-            txtTitle.TextChanged += new EventHandler(txtTitle_TextChanged);
-            txtFirstName.TextChanged += new EventHandler(txtFirstName_TextChanged);
-            txtLastName.TextChanged += new EventHandler(txtLastName_TextChanged);
-            txtSex.TextChanged += new EventHandler(txtSex_TextChanged);
-            txtNationality.TextChanged += new EventHandler(txtNationality_TextChanged);
-            txtSocialSecurityNumber.TextChanged += new EventHandler(txtSocialSecurityNumber_TextChanged);
-            txtDateOfBirth.TextChanged += new EventHandler(txtDateOfBirth_TextChanged);
+            txtSalutation.Enabled = true;
+            txtTitle.Enabled = true;
+            txtFirstName.Enabled = true;
+            txtLastName.Enabled = true;
+            txtSex.Enabled = true;
+            txtNationality.Enabled = true;
+            txtSocialSecurityNumber.Enabled = true;
+            txtDateOfBirth.Enabled = true;
+            txtStreet.Enabled = true;
+            txtStreetNumber.Enabled = true;
+            txtPlace.Enabled = true;
+            numZipCode.Enabled = true;
+            txtPhoneNumberMobile.Enabled = true;
+            txtPhoneNumberPrivate.Enabled = true;
+            txtPhoneNumberBusiness.Enabled = true;
+            txtEmail.Enabled = true;
 
-            // Address-related text boxes need to check a single checkbox
-            txtStreet.TextChanged += new EventHandler(txtAdress_TextChanged);
-            txtStreetNumber.TextChanged += new EventHandler(txtAdress_TextChanged);
-            txtPlace.TextChanged += new EventHandler(txtAdress_TextChanged);
-            txtZipCode.TextChanged += new EventHandler(txtAdress_TextChanged);
+            if (_currentUser.GetType() != typeof(Employee))
+            {
+                txtStartDate.Enabled = false;
+                txtStartDate.Value = new DateTime(1900, 1, 1);
+                txtEndDate.Enabled = false;
+                txtEndDate.Value = new DateTime(1900, 1, 1);
+                numEmployment.Enabled = false;
+                numEmployment.Text = "";
+                txtDepartment.Enabled = false;
+                txtDepartment.Text = "";
+                txtRole.Enabled = false;
+                txtRole.Text = "";
+                txtCadreLevel.Enabled = false;
+                txtCadreLevel.Text = "";
+            }
 
-            // Phone number related
-            txtPhoneNumberMobile.TextChanged += new EventHandler(txtPhoneNumberMobile_TextChanged);
-            txtPhoneNumberPrivate.TextChanged += new EventHandler(txtPhoneNumberPrivate_TextChanged);
-            txtPhoneNumberBusiness.TextChanged += new EventHandler(txtPhoneNumberBusiness_TextChanged);
-            txtEmail.TextChanged += new EventHandler(txtEmail_TextChanged);
+            if (_currentUser.GetType() != typeof(Trainee))
+            {
+                btnTraineeYearsEdit.Checked = false;
+                numTraineeYears.Enabled = false;
+                numActualTraineeYear.Enabled = false;
+            }
+            if (_currentUser.GetType() != typeof(Customer))
+            {
+                txtCompanyName.Enabled = false;
+                txtCompanyName.Text = "";
+                txtCompanyType.Enabled = false;
+                txtCompanyType.Text = "";
+                txtCompanyContact.Enabled = false;
+                txtCompanyContact.Text = "";
+            }
+        }
+        private void FillTextFields()
+        {
+            if (_currentUser.Status == true) checkBox1.Checked = true;
+            txtSalutation.Text = _currentUser.Salutation;
+            txtTitle.Text = _currentUser.Title;
+            txtFirstName.Text = _currentUser.FirstName;
+            txtLastName.Text = _currentUser.LastName;
+            txtSex.Text = _currentUser.Gender;
+            txtNationality.Text = _currentUser.Nationality;
+            txtSocialSecurityNumber.Text = _currentUser.SocialSecurityNumber;
+            txtDateOfBirth.Value = _currentUser.DateOfBirth ?? new DateTime(1900,1,1);
+            txtStreet.Text = _currentUser.Street;
+            txtStreetNumber.Text = _currentUser.StreetNumber;
+            txtPlace.Text = _currentUser.Place;
+            if (_currentUser.ZipCode != null) numZipCode.Value = (decimal)_currentUser.ZipCode;
+            txtPhoneNumberMobile.Text = _currentUser.PhoneNumberMobile;
+            txtPhoneNumberPrivate.Text = _currentUser.PhoneNumberPrivate;
+            txtPhoneNumberBusiness.Text = _currentUser.PhoneNumberBusiness;
+            txtEmail.Text = _currentUser.Email;
 
-            // Employment and Department related
-            txtEmployment.TextChanged += new EventHandler(txtEmployment_TextChanged);
-            txtDepartment.TextChanged += new EventHandler(txtDepartment_TextChanged);
-
-            // Trainee related
-            btnTrainee.CheckedChanged += new EventHandler(btnTrainee_CheckedChanged);
-            numTraineeYears.ValueChanged += new EventHandler(numTraineeYears_ValueChanged);
-            numActualTraineeYear.ValueChanged += new EventHandler(numActualTraineeYear_ValueChanged);
-
-            // Start and End Date related text boxes need to check a single checkbox
-            txtStartDate.TextChanged += new EventHandler(txtStartEndDate_TextChanged);
-            txtEndDate.TextChanged += new EventHandler(txtStartEndDate_TextChanged);
-
-            txtRole.TextChanged += new EventHandler(txtRole_TextChanged);
-            txtCadreLevel.TextChanged += new EventHandler(txtCadreLevel_TextChanged);
-            txtCompanyName.TextChanged += new EventHandler(txtCompanyName_TextChanged);
-            txtCompanyType.TextChanged += new EventHandler(txtCompanyType_TextChanged);
-            txtCompanyContact.TextChanged += new EventHandler(txtCompanyContact_TextChanged);
+            if (_currentUser.GetType() == typeof(Employee))
+            {
+                Employee employee = (Employee)_currentUser;
+                txtStartDate.Value = employee.StartDate ?? new DateTime(1900, 1, 1);
+                txtEndDate.Value = employee.EndDate ?? new DateTime(1900, 1, 1);
+                if (employee.Employment != null) numEmployment.Value = (decimal)employee.Employment;
+                txtDepartment.Text = employee.Departement;
+                txtRole.Text = employee.Role;
+                txtCadreLevel.Text = employee.CadreLevel.ToString();
+            }
+            if (_currentUser.GetType() == typeof(Trainee))
+            {
+                Trainee trainee = (Trainee)_currentUser;
+                if (trainee.TraineeYears != null) numTraineeYears.Value = (decimal)trainee.TraineeYears;
+                if (trainee.ActualTraineeYear != null) numActualTraineeYear.Value = (decimal)trainee.ActualTraineeYear;
+            }
+            if (_currentUser.GetType() == typeof(Customer))
+            {
+                Customer customer = (Customer)_currentUser;
+                txtCompanyName.Text = customer.CompanyName;
+                txtCompanyType.Text = customer.CompanyType;
+                txtCompanyContact.Text = customer.CompanyContact;
+            }
         }
 
-        // Event handlers for each TextBox-CheckBox pair
-        private void txtSalutation_TextChanged(object sender, EventArgs e)
+        private void btnEditUser_Click(object sender, EventArgs e)
         {
-            btnSalutationEdit.Checked = !string.IsNullOrWhiteSpace(txtSalutation.Text);
-        }
 
-        private void txtTitle_TextChanged(object sender, EventArgs e)
-        {
-            btnTitleEdit.Checked = !string.IsNullOrWhiteSpace(txtTitle.Text);
-        }
+            if (_currentUser.GetType() == typeof(Trainee))
+            {
+                _updatedUser = new Trainee()
+                {
+                    TraineeYears = (int)numTraineeYears.Value,
+                    ActualTraineeYear = (int)numActualTraineeYear.Value,
+                    StartDate = txtStartDate.Value,
+                    EndDate = txtEndDate.Value,
+                    Employment = (int)numEmployment.Value,
+                    Departement = txtDepartment.Text,
+                    Role = txtRole.Text,
+                    CadreLevel = (int)txtCadreLevel.Value
+                };
+            }
+            else if (_currentUser.GetType() == typeof(Employee))
+            {
+                _updatedUser = new Employee()
+                {
+                    StartDate = txtStartDate.Value,
+                    EndDate = txtEndDate.Value,
+                    Employment = (int)numEmployment.Value,
+                    Departement = txtDepartment.Text,
+                    Role = txtRole.Text,
+                    CadreLevel = (int)txtCadreLevel.Value
+                };
+            }
+            else if (_currentUser.GetType() == typeof(Customer))
+            {
+                _updatedUser = new Customer()
+                {
+                    CompanyName = txtCompanyName.Text,
+                    CompanyType = txtCompanyType.Text,
+                    CompanyContact = txtCompanyContact.Text
+                };
+            }
+            _updatedUser.Status = checkBox1.Checked;
+            _updatedUser.Salutation = txtSalutation.Text;
+            _updatedUser.Title = txtTitle.Text;
+            _updatedUser.FirstName = txtFirstName.Text;
+            _updatedUser.LastName = txtLastName.Text;
+            _updatedUser.Gender = txtSex.Text;
+            _updatedUser.Nationality = txtNationality.Text;
+            _updatedUser.SocialSecurityNumber = txtSocialSecurityNumber.Text;
+            _updatedUser.DateOfBirth = txtDateOfBirth.Value;
+            _updatedUser.Street = txtStreet.Text;
+            _updatedUser.StreetNumber = txtStreetNumber.Text;
+            _updatedUser.Place = txtPlace.Text;
+            _updatedUser.ZipCode = (int)numZipCode.Value;
+            _updatedUser.PhoneNumberMobile = txtPhoneNumberMobile.Text;
+            _updatedUser.PhoneNumberPrivate = txtPhoneNumberPrivate.Text;
+            _updatedUser.PhoneNumberBusiness = txtPhoneNumberBusiness.Text;
+            _updatedUser.Email = txtEmail.Text;
 
-        private void txtFirstName_TextChanged(object sender, EventArgs e)
-        {
-            btnFirstNameEdit.Checked = !string.IsNullOrWhiteSpace(txtFirstName.Text);
-        }
+            if(!_currentUser.Contains(_updatedUser))
+            {
+                _userController.Update(_currentUser, _updatedUser);
+            }
 
-        private void txtLastName_TextChanged(object sender, EventArgs e)
-        {
-            btnLastNameEdit.Checked = !string.IsNullOrWhiteSpace(txtLastName.Text);
-        }
-
-        private void txtSex_TextChanged(object sender, EventArgs e)
-        {
-            btnSexEdit.Checked = !string.IsNullOrWhiteSpace(txtSex.Text);
-        }
-
-        private void txtNationality_TextChanged(object sender, EventArgs e)
-        {
-            btnNationalityEdit.Checked = !string.IsNullOrWhiteSpace(txtNationality.Text);
-        }
-
-        private void txtSocialSecurityNumber_TextChanged(object sender, EventArgs e)
-        {
-            btnSocialSecurityNumberEdit.Checked = !string.IsNullOrWhiteSpace(txtSocialSecurityNumber.Text);
-        }
-
-        private void txtDateOfBirth_TextChanged(object sender, EventArgs e)
-        {
-            btnDateOfBirthEdit.Checked = !string.IsNullOrWhiteSpace(txtDateOfBirth.Text);
-        }
-
-        // Event handler for Address fields: Checkbox is checked if any field is filled
-        private void txtAdress_TextChanged(object sender, EventArgs e)
-        {
-            btnAdressEdit.Checked = !string.IsNullOrWhiteSpace(txtStreet.Text) ||
-                                    !string.IsNullOrWhiteSpace(txtStreetNumber.Text) ||
-                                    !string.IsNullOrWhiteSpace(txtPlace.Text) ||
-                                    !string.IsNullOrWhiteSpace(txtZipCode.Text);
-        }
-
-        private void txtPhoneNumberMobile_TextChanged(object sender, EventArgs e)
-        {
-            btnPhoneNumberMobileEdit.Checked = !string.IsNullOrWhiteSpace(txtPhoneNumberMobile.Text);
-        }
-
-        private void txtPhoneNumberPrivate_TextChanged(object sender, EventArgs e)
-        {
-            btnPhoneNumberPrivateEdit.Checked = !string.IsNullOrWhiteSpace(txtPhoneNumberPrivate.Text);
-        }
-
-        private void txtPhoneNumberBusiness_TextChanged(object sender, EventArgs e)
-        {
-            btnPhoneNumberBusinessEdit.Checked = !string.IsNullOrWhiteSpace(txtPhoneNumberBusiness.Text);
-        }
-
-        private void txtEmail_TextChanged(object sender, EventArgs e)
-        {
-            btnEmailEdit.Checked = !string.IsNullOrWhiteSpace(txtEmail.Text);
-        }
-
-        private void txtEmployment_TextChanged(object sender, EventArgs e)
-        {
-            btnEmplyomentTypeEdit.Checked = !string.IsNullOrWhiteSpace(txtEmployment.Text);
-        }
-
-        private void txtDepartment_TextChanged(object sender, EventArgs e)
-        {
-            btnDepartementEdit.Checked = !string.IsNullOrWhiteSpace(txtDepartment.Text);
-        }
-
-        private void txtRole_TextChanged(object sender, EventArgs e)
-        {
-            btnRoleEdit.Checked = !string.IsNullOrWhiteSpace(txtRole.Text);
-        }
-
-        private void txtCadreLevel_TextChanged(object sender, EventArgs e)
-        {
-            btnCadreLevelEdit.Checked = !string.IsNullOrWhiteSpace(txtCadreLevel.Text);
-        }
-
-        // Check if either Start Date or End Date has text
-        private void txtStartEndDate_TextChanged(object sender, EventArgs e)
-        {
-            btnStartEndDateEdit.Checked = !string.IsNullOrWhiteSpace(txtStartDate.Text) ||
-                                          !string.IsNullOrWhiteSpace(txtEndDate.Text);
-        }
-
-        // Event handler for Trainee checkbox state change
-        private void btnTrainee_CheckedChanged(object sender, EventArgs e)
-        {
-            bool isChecked = btnTrainee.Checked;
-            numActualTraineeYear.Enabled = isChecked;
-            numTraineeYears.Enabled = isChecked;
-        }
-
-        // Event handler for Trainee Years
-        private void numTraineeYears_ValueChanged(object sender, EventArgs e)
-        {
-            btnTraineeYearsEdit.Checked = numTraineeYears.Value > 0;
-        }
-
-        // Event handler for Actual Trainee Year
-        private void numActualTraineeYear_ValueChanged(object sender, EventArgs e)
-        {
-            btnActualTraineeYearEdit.Checked = numActualTraineeYear.Value > 0;
-        }
-
-        private void txtCompanyName_TextChanged(object sender, EventArgs e)
-        {
-            btnCompanyNameEdit.Checked = !string.IsNullOrWhiteSpace(txtCompanyName.Text);
-        }
-
-        private void txtCompanyType_TextChanged(object sender, EventArgs e)
-        {
-            btnCompanyTypeEdit.Checked = !string.IsNullOrWhiteSpace(txtCompanyType.Text);
-        }
-
-        private void txtCompanyContact_TextChanged(object sender, EventArgs e)
-        {
-            btnCompanyContactEdit.Checked = !string.IsNullOrWhiteSpace(txtCompanyContact.Text);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
         }
     }
 }
