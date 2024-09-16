ab7f2869c4b720b19b3b42c0418b188990a834e0
Pedro Ribeiro <beyluta1@gmail.com>
2024-09-16 10:08:35 -0700

    add export feature

--- ZbW_P_Contact_Manager/UI/frmImportExport.cs
+++ ZbW_P_Contact_Manager/UI/frmImportExport.cs
@@ -8,11 +8,13 @@ namespace ZbW_P_Contact_Manager.UI
 {
     public partial class frmImportExport : Form
     {
+        UserController _userController;
         CSVController _csvController;
 
         public frmImportExport()
         {
             InitializeComponent();
+            _userController = new UserController();
             _csvController = new CSVController();
         }
 
@@ -33,14 +35,40 @@ namespace ZbW_P_Contact_Manager.UI
             }
         }
 
+        private void ExportModel(ModelType modelType)
+        {
+            string filePath = GetModelPathByType(modelType);
+            if (!File.Exists(filePath))
+                MessageBox.Show(SystemMessage.GetMessage(Error.ModelFileMissing));
+            else
+            {
+                string folderPath = GetFolderPathViaDialog();
+                File.Copy(filePath, $"{folderPath}\\{modelType}.csv");
+            }
+        }
+
+        private string GetModelPathByType(ModelType modelType)
+        {
+            return modelType switch
+            {
+                ModelType.Employee => _csvController.GetPathByModelType(typeof(Employee)),
+                ModelType.Trainee => _csvController.GetPathByModelType(typeof(Trainee)),
+                ModelType.Customer or _ => _csvController.GetPathByModelType(typeof(Customer)),
+            };
+        }
+
+        private string GetFolderPathViaDialog()
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                return dialog.SelectedPath;
+            }
+        }
+
         private void btnImportEmployee_Click(object sender, EventArgs e)
         {
             ImportModel(ModelType.Employee);
-        }
-
-        private void btnExportEmployee_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void btnImportCustomer_Click(object sender, EventArgs e)
@@ -48,9 +76,19 @@ namespace ZbW_P_Contact_Manager.UI
             ImportModel(ModelType.Customer);
         }
 
+        private void btnExportEmployee_Click(object sender, EventArgs e)
+        {
+            ExportModel(ModelType.Employee);
+        }
+
         private void btnExportCustomer_Click(object sender, EventArgs e)
         {
-               
+            ExportModel(ModelType.Customer);
+        }
+
+        private void btnExportTrainee_Click(object sender, EventArgs e)
+        {
+            ExportModel(ModelType.Trainee);
         }
     }
 }
